0997706ed68d03bcbc5bacd9577b0736cc85a4d6
mrtega <tegaodiete@gmail.com>
2021-01-10 17:33:52 +0100

    Added AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProcessControlApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessControlApplication.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Models.Action> Actions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationRole>().HasIndex(x => x.ParentRoleId).IsUnique();
+            builder.Entity<ApplicationRole>().HasIndex(x => x.ChildRoleId).IsUnique();
+        }
+    }
+}
