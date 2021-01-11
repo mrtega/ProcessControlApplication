6cd4a27c3e759670bbc02089d0535b388f6f7b38
mrtega <tegaodiete@gmail.com>
2021-01-11 14:16:12 +0100

    Updated AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -25,6 +25,38 @@ namespace ProcessControlApplication.Data
             base.OnModelCreating(builder);
             builder.Entity<ApplicationRole>().HasIndex(x => x.ParentRoleId).IsUnique();
             builder.Entity<ApplicationRole>().HasIndex(x => x.ChildRoleId).IsUnique();
+            builder.Entity<ApplicationRole>().HasData(
+                new ApplicationRole
+                {
+                    Id = 1,
+                    Name = "StoreKeeper",
+                    ChildRoleId = 2,
+                    NormalizedName = "STOREKEEPER"
+                },
+                new ApplicationRole
+                {
+                    Id = 2,
+                    Name = "Supervisor",
+                    ChildRoleId = 3,
+                    ParentRoleId = 1,
+                    NormalizedName = "SUPERVISOR"
+                },
+                new ApplicationRole
+                {
+                    Id = 3,
+                    Name = "ProductManager",
+                    ChildRoleId = 4,
+                    ParentRoleId = 2,
+                    NormalizedName = "PRODUCTMANAGER"
+                },
+                new ApplicationRole
+                {
+                    Id = 4,
+                    Name = "Manager",
+                    ParentRoleId = 3,
+                    NormalizedName = "MANAGER"
+                }
+                );
         }
     }
 }
---
+++ Migrations/20210111125422_Updated seeding data.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProcessControlApplication.Migrations
+{
+    public partial class Updatedseedingdata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "NormalizedName" },
+                values: new object[] { "95db901e-60fd-4111-81cf-17cbe2fce95f", "STOREKEEPER" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ConcurrencyStamp", "NormalizedName" },
+                values: new object[] { "6951f1ec-b131-408b-9445-dafd9a9812a1", "SUPERVISOR" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "NormalizedName" },
+                values: new object[] { "10854c8b-3c36-4405-a29a-8b5fe6c127f0", "PRODUCTMANAGER" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "62e65677-b649-4b1b-a36f-a50bf53ee084", "Manager", "MANAGER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "NormalizedName" },
+                values: new object[] { "5ca0c5e2-1c8a-4722-896d-0669f7553c2d", null });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ConcurrencyStamp", "NormalizedName" },
+                values: new object[] { "a9cd77ce-25d6-4c22-9efb-3ccf9c39dab1", null });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "NormalizedName" },
+                values: new object[] { "c83073aa-2529-44b6-9efd-da53b8f0d0b4", null });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "0b12b5b9-9ab5-4b52-bdaa-6732a4f9274e", "ProductManager", null });
+        }
+    }
+}
